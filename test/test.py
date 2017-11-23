f9e7a177c022975edbad9800ed784d6316b562be
Anita Schürch <anita.schuerch@gmail.com>
2017-11-23 07:28:41 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -13,12 +13,11 @@ shutil.copy (cwd+"/test/Test_R2.fastq.gz", cwd)
 process = subprocess.Popen(['bash','bactofidia.sh', 'Test'], stdout=subprocess.PIPE)
 stdout = process.communicate()[0]
 print ('STDOUT:{}'.format(stdout))
-
-
-
-#can I catch the exit status of the subprocess?
+rc = process.returncode
 
 os.remove ("Test_R1.fastq.gz")
 os.remove ("Test_R2.fastq.gz")
 
-
+# exit with exitcode of subprocess bactofidia
+sys.exit(rc)
+    
