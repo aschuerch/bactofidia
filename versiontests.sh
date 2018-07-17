a4d81487a89338a5bb56acd578edc67bc1ba7d19
Anita Schurch <anita.schuerch@gmail.com>
2018-07-17 07:57:18 +0200

    ongoing adjustments

--- versiontests.sh
+++ versiontests.sh
@@ -6,7 +6,7 @@ set -e
 # Datasets and spades versions can be adjusted
 data=Test
 
-for version in 3.5.0 3.6.2 3.7.0 3.8.0 3.8.1 3.9.0 3.10.0 3.10.1  
+for version in 3.11.0 3.11.1 3.12.0  
 do
 echo "$version" 
 if [ ! -d bactofidia_"$version" ]; then
