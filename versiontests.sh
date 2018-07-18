c264b3883af3c005e0114e54cc4d3579e3d77da1
Anita Schurch <anita.schuerch@gmail.com>
2018-07-18 10:19:01 +0200

    versiontests debugged

--- versiontests.sh
+++ versiontests.sh
@@ -4,21 +4,24 @@ set -e
 
 # small script to compare different spades versions
 # Datasets and spades versions can be adjusted
-data=Test
+# Data is contained in
+datasets=/home/dla_mm/aschurch/data/2018_07_17_SPADesTestRepeats/data
 
 for version in 3.5.0 3.6.2 3.7.0 3.8.0 3.8.1 3.9.0 3.10.0 3.10.1 3.11.0 3.11.1 3.12.0
 do
-echo "$version" 
-if [ ! -d bactofidia_"$version" ]; then
-git clone https://github.com/aschuerch/bactofidia bactofidia_"$version"
-fi
-cd bactofidia_"$version"
-git checkout -b newestversions remotes/origin/newestversions
-ln -fs ../*fastq.gz .
+  echo "$version" 
+  if [ ! -d bactofidia_"$version" ]; then
+     git clone https://github.com/aschuerch/bactofidia bactofidia_"$version"
+  fi
+  cd bactofidia_"$version"
+  git checkout -b newestversions remotes/origin/newestversions
 
-sed -i s/spades\=3\.12\.0/spades\=$version/g  envs/spades.yaml 
-done
+  ln -s "$datasets"/*fastq.gz .
 
-./bactofidia.sh "$data"
-cd ..
+  sed -i s/spades\=3\.12\.0/spades\=$version/g  envs/spades.yaml 
+
+  ./bactofidia.sh ALL
+
+  cd ..
+
 done
