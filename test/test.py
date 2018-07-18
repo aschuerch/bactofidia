0f9cfe58683f3f4571d5f7ab7c226c22236817e4
Anita Schurch <anita.schuerch@gmail.com>
2018-07-18 20:35:41 +0200

    test script fix to match new pattern

--- test/test.py
+++ test/test.py
@@ -10,7 +10,7 @@ cwd = os.getcwd()
 shutil.copy (cwd+"/test/Test_R1.fastq.gz", cwd)
 shutil.copy (cwd+"/test/Test_R2.fastq.gz", cwd)
 
-process = subprocess.Popen(['bash','bactofidia.sh', 'Test'], stdout=subprocess.PIPE)
+process = subprocess.Popen(['bash','bactofidia.sh', 'Test*'], stdout=subprocess.PIPE)
 stdout = process.communicate()[0]
 print ('STDOUT:{}'.format(stdout))
 rc = process.returncode
