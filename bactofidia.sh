18e4b9797491cc574661faf3d198e2717c40baae
Anita Schurch <anita.schuerch@gmail.com>
2018-09-20 10:00:54 +0200

    another shot at channel config

--- bactofidia.sh
+++ bactofidia.sh
@@ -93,9 +93,8 @@ else
  export PATH=~/tmp/Miniconda3/bin:$PATH 
  export PYTHONPATH=~/tmp/Miniconda3/pkgs/
  conda config --add channels conda-forge
- conda config --add channels defaults
- conda config --add channels r
  conda config --add channels bioconda
+ conda config --add channels defaults
  export PERL5LIB="~/tmp/Miniconda3/lib/perl5/site_perl/5.22.0"
 fi
 
