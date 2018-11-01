ba86651c8e4282e6695b698c45bcb23ca6ce71a9
Anita Schurch <anita.schuerch@gmail.com>
2018-11-01 08:22:07 +0100

    config to own folder

--- dryrun.sh
+++ dryrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source activate snakemake
-snakemake -np  --snakefile Snakefile.assembly --config configfile=config.yaml
+snakemake -np  --snakefile Snakefile.assembly --config configfile=config/config.yaml
 source deactivate
 
--- unlock.sh
+++ unlock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # command to unlock the workflow
 source activate snakemake
-snakemake --snakefile Snakefile.assembly --unlock --config configfile=config.yaml
+snakemake --snakefile Snakefile.assembly --unlock --config configfile=config/config.yaml
 source deactivate
