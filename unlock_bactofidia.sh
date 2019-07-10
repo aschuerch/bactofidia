0057d6e697ed2b933fa46e7519da810926cbbe13
Anita Schuerch <anita.schuerch@gmail.com>
2019-07-10 16:49:33 +0200

    removes without asksin

--- unlock_bactofidia.sh
+++ unlock_bactofidia.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # command to unlock the workflow
 source activate snakemake
+
+#remove all generated temporary directories if any
+for i in tmp data stats results log
+ do
+ rm -r "$i"
+ done
+
+
 snakemake --snakefile Snakefile.assembly --unlock --config configfile=config/config.yaml
 source deactivate
