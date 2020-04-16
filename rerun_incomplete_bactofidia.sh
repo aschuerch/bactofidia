1fc994953fbdc29c5822a76c79604c7e0ebb1cd3
Anita Schurch <anita.schuerch@gmail.com>
2020-04-16 08:58:04 +0200

    removed sge

--- rerun_incomplete_bactofidia.sh
+++ rerun_incomplete_bactofidia.sh
@@ -9,26 +9,26 @@ echo "snakemake will be installed"
 conda install -y snakemake
 fi
 
-if command -v qstat > /dev/null; then
+#if command -v qstat > /dev/null; then
 
-snakemake \
-  --snakefile Snakefile.assembly \
-  --latency-wait 60 \
-  --config configfile=config/config.yaml \
-  --rerun-incomplete \
-  --verbose \
-  --forceall \
-  --keep-going \
-  --restart-times 5\
-  --use-conda \
-  --cluster \
-  'qsub -V -cwd -l h_vmem=32G -l h_rt=04:00:00 -e log/ -o log/ ' \
-  --jobs 100
+#snakemake \
+#  --snakefile Snakefile.assembly \
+#  --latency-wait 60 \
+#  --config configfile=config/config.yaml \
+#  --rerun-incomplete \
+#  --verbose \
+#  --forceall \
+#  --keep-going \
+#  --restart-times 5\
+#  --use-conda \
+#  --cluster \
+#  'qsub -V -cwd -l h_vmem=32G -l h_rt=04:00:00 -e log/ -o log/ ' \
+#  --jobs 100
 
-else
+#else
 snakemake --snakefile Snakefile.assembly --use-conda --printshellcmds  --config configfile=config/config.yaml --rerun-incomplete
 
-fi
+#fi
 
 conda deactivate
 
