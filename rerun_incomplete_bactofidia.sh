2492f225f3f52e532c3ab3c5083bae4f4e44cf05
Rodrigo Meneses <me.groodri@gmail.com>
2021-02-08 20:19:36 +0000

    Log

--- rerun_incomplete_bactofidia.sh
+++ rerun_incomplete_bactofidia.sh
@@ -9,6 +9,16 @@ echo "snakemake will be installed"
 conda install -y snakemake=5.14.0
 fi
 
-snakemake --snakefile Snakefile.assembly  --cores all --use-conda --printshellcmds  --config configfile=config/config.yaml --rerun-incomplete
+# run snakemake just to unlock the folder
+
+snakemake --snakefile Snakefile.assembly --cores all --use-conda  --printshellcmds --config configfile=config/config.yaml --unlock
 
 
+## reuse log file
+
+log=$(pwd)/log/call_assembly.txt
+
+
+## re-run the snakemake pipeline
+
+snakemake --snakefile Snakefile.assembly  --cores all --use-conda --printshellcmds  --config configfile=config/config.yaml --rerun-incomplete 2>&1 | tee -a "$log"
