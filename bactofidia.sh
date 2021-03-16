ddbd7b462e0b9fa0329a99e01c8da352d60afd87
Rodrigo Meneses <me.groodri@gmail.com>
2021-03-16 15:08:05 +0100

    Changed snakemake latency to 60s

--- bactofidia.sh
+++ bactofidia.sh
@@ -158,8 +158,8 @@ for file in "${files[@]}"
 
 # run the snakemake pipeline
 
-echo "snakemake --snakefile Snakefile.assembly --cores all --use-conda --printshellcmds --keep-going --config configfile=""$configfile"
-snakemake --snakefile Snakefile.assembly --use-conda  --cores all --printshellcmds  --keep-going --config configfile="$configfile" 2>&1 | tee -a "$log"
+echo "snakemake --snakefile Snakefile.assembly --use-conda --cores all --printshellcmds --latency-wait 60 --keep-going --config configfile=""$configfile"
+snakemake --snakefile Snakefile.assembly --use-conda --cores all --printshellcmds --latency-wait 60 --keep-going --config configfile="$configfile" 2>&1 | tee -a "$log"
 
 #for the CI
 if [ $? -eq 0 ]
